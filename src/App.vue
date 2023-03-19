254ad4810b5ac747d85a67b3015988e8427a653a
Dmitriy <d.deshura@yandex.ru>
2023-03-19 06:29:02 +0300

    19.03 first homework

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import { reactive, ref } from 'vue';
+  import Timer from "./components/Timer.vue"
+
+    // не работает, (разными способами пытался продублировать строку по числу кликов)
+    let clicks = ref(0)
+    let buttonText = ref("Hip hip hooray!!!".repeat(clicks.value + 1))
+    // const buttonText = reactive ({
+    //   text: "Hip hip hooray \n".repeat(clicks.value + 1),
+    // })
+
+</script>
+
+<template>
+
+  <div class="main">
+    <timer></timer>
+
+    <div class="content_1">
+      <button class="hoorayButton" @click="clicks ++">
+        <img class="button_picture" src="/button.png"><div class="buttonText"> {{ clicks }} clicked <br/> {{ buttonText }} <br/> :3 </div>
+      </button>
+      <div class="content_1_text"></div>
+    </div>
+
+  </div>
+  
+</template>
+
+<style scoped></style>
